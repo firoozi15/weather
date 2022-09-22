9b98a3ab4e3cc9b9e0931b1becaa33d49dc282de
Abolfazl Firoozi <abolffff1383@gmail.com>
2022-09-22 12:07:08 +0330

    added Weatherdata in html page with js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const form = document.querySelector("#head form");
 const input = document.querySelector("#text");
 const msg = document.querySelector("#msg");
 const list = document.querySelector("#Show-weader #Show-weader-list");
-
 const apikey = "bb12c11782c6d9aceacb80db95c8d741";
 
 form.addEventListener("submit", e => {
@@ -13,9 +12,28 @@ form.addEventListener("submit", e => {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const {main,name,sys,weather} = data;
-            let icon = `http://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
-            const li = document.createElement("li");
-            li.classList.add("card-weather");
+            msg.innerText = "";
+            try {
+                const {main,name,sys,weather} = data;
+                let icon = `http://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+                const li = document.createElement("li");
+                li.classList.add("card-weather");
+                const elementsCardWeather = `
+                <div class="countryName">${sys["country"]}</div>
+                <div class="cityName">${name}</div>
+                <div class="cityTemp">
+                    <div class="temp">${main["temp"]}</div>
+                    <sup>°C</sup>
+                </div>
+                <img src="${icon}" alt="weatherIcon">
+                <p>${weather[0]["description"]}</p>
+                `;
+    
+                li.innerHTML = elementsCardWeather;
+                list.appendChild(li);
+                input.value = "";
+            } catch{
+                msg.innerText = "This value is`t valid";                
+            }
         })
 })
